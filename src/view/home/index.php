292585504277f06993476798f82c3e2d6dd8ee7a
Lara Tomme <lara.tomme@gmail.com>
2021-01-29 19:16:15 +0100

    CSS Home #17

--- src/view/home/index.php
+++ src/view/home/index.php
@@ -1,3 +1,6 @@
+<h1>Licht en Liefde Platform</h1>
+<p>Vind informatie op het webplatform van Netwerk Licht en Liefde.</p>
+
 <div>
     <?php foreach ($categories as $category) : ?>
         <a href="index.php?page=category&id=<?php echo $category['CategoryID'] ?>">
@@ -10,4 +13,22 @@
             </div>
         </a>
     <?php endforeach ?>
+</div>
+
+<h2>Vragen of problemen?</h2>
+<p>Contacteer Netwerk Licht en Liefde of chat met een hulpverlener.</p>
+
+<div class="flex">
+    <div class="card-contact">
+        <div>Icon</div>
+        <div class="card-contact--yellow">
+            <p>Contact formulier</p>
+        </div>
+    </div>
+    <div class="card-contact">
+        <div>Icon</div>
+        <div class="card-contact--yellow">
+            <p>Chat</p>
+        </div>
+    </div>
 </div>
