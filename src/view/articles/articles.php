a82ad748862c50264510e88d4bbb92f0fd294f7a
Lara Tomme <lara.tomme@gmail.com>
2021-01-31 18:53:04 +0100

    CSS Artikels Detail/ Bewerken (Beheerder) #31

--- src/view/articles/articles.php
+++ src/view/articles/articles.php
@@ -65,74 +65,84 @@
 
 <?php } else { ?>
     <h1>Artikel</h1>
+    <a class="button-link" href="index.php?page=articles">
+        <div class="button-blue">
+            <p>Terug</p>
+        </div>
+    </a>
     <!-- Detail -->
-    <div>
+    <div class="artikels-form">
         <form action="index.php?page=articles" method="post">
-            <input type="hidden" name="id" value="<?php if (!empty($article['ArticleID'])) {
-                                                        echo $article['ArticleID'];
-                                                    } ?>" />
-            <div>
-                <label for="title">Titel</label>
-                <input id="title" type="text" name="title" placeholder="Titel" value="<?php if (!empty($article['Title'])) {
-                                                                                            echo $article['Title'];
-                                                                                        } ?>" minlength="3" maxlength="64" required />
+            <div class="form-grid artikel-form-input">
+                <input type="hidden" name="id" value="<?php if (!empty($article['ArticleID'])) {
+                                                            echo $article['ArticleID'];
+                                                        } ?>" />
+
+                <div class="form-grid-items">
+                    <label for="title">Titel</label>
+                    <input id="title" type="text" name="title" placeholder="Titel" value="<?php if (!empty($article['Title'])) {
+                                                                                                echo $article['Title'];
+                                                                                            } ?>" minlength="3" maxlength="64" required />
+                </div>
+                <div class="form-grid-items">
+                    <label for="description">Omschrijving</label>
+                    <input id="description" type="text" name="description" placeholder="Omschrijving" value="<?php if (!empty($article['Description'])) {
+                                                                                                                    echo $article['Description'];
+                                                                                                                } ?>" minlength="3" maxlength="256" />
+                </div>
+                <div class="form-grid-items">
+                    <label for="articletypeid">Artikel Type</label>
+                    <select id="articletypeid" name="articletypeid">
+                        <option value> -- Kies een Artikel Type -- </option>
+                        <?php foreach ($articletypes as $articletype) : ?>
+                            <option value="<?php echo $articletype['ArticleTypeID']; ?>" <?php if (!empty($article['ArticleTypeID'])) {
+                                                                                                echo $articletype['ArticleTypeID'] === $article['ArticleTypeID'] ? "selected" : "";
+                                                                                            } ?>><?php echo $articletype['Name'] ?></option>
+                        <?php endforeach ?>
+                    </select>
+                </div>
+                <div class="form-grid-items">
+                    <label for="categoryid">Category</label>
+                    <select id="categoryid" name="categoryid">
+                        <option value> -- Kies een Category -- </option>
+                        <?php foreach ($categories as $category) : ?>
+                            <option value="<?php echo $category['CategoryID']; ?>" <?php if (!empty($article['CategoryID'])) {
+                                                                                        echo $category['CategoryID'] === $article['CategoryID'] ? "selected" : "";
+                                                                                    } ?>><?php echo $category['Name'] ?></option>
+                        <?php endforeach ?>
+                    </select>
+                </div>
+                <div class="form-grid-items">
+                    <label for="usergroupid">Gebruikergroep</label>
+                    <select id="usergroupid" name="usergroupid">
+                        <option value>Iedereen</option>
+                        <?php foreach ($usergroups as $usergroup) : ?>
+                            <option value="<?php echo $usergroup['UserGroupID']; ?>" <?php if (!empty($article['UserGroupID'])) {
+                                                                                            echo $usergroup['UserGroupID'] === $article['UserGroupID'] ? "selected" : "";
+                                                                                        } ?>><?php echo $usergroup['Name'] ?></option>
+                        <?php endforeach ?>
+                    </select>
+                </div>
             </div>
-            <div>
-                <label for="description">Omschrijving</label>
-                <input id="description" type="text" name="description" placeholder="Omschrijving" value="<?php if (!empty($article['Description'])) {
-                                                                                                                echo $article['Description'];
-                                                                                                            } ?>" minlength="3" maxlength="256" />
-            </div>
-            <div>
-                <label for="articletypeid">Artikel Type</label>
-                <select id="articletypeid" name="articletypeid">
-                    <option value> -- Kies een Artikel Type -- </option>
-                    <?php foreach ($articletypes as $articletype) : ?>
-                        <option value="<?php echo $articletype['ArticleTypeID']; ?>" <?php if (!empty($article['ArticleTypeID'])) {
-                                                                                            echo $articletype['ArticleTypeID'] === $article['ArticleTypeID'] ? "selected" : "";
-                                                                                        } ?>><?php echo $articletype['Name'] ?></option>
-                    <?php endforeach ?>
-                </select>
-            </div>
-            <div>
-                <label for="categoryid">Category</label>
-                <select id="categoryid" name="categoryid">
-                    <option value> -- Kies een Category -- </option>
-                    <?php foreach ($categories as $category) : ?>
-                        <option value="<?php echo $category['CategoryID']; ?>" <?php if (!empty($article['CategoryID'])) {
-                                                                                    echo $category['CategoryID'] === $article['CategoryID'] ? "selected" : "";
-                                                                                } ?>><?php echo $category['Name'] ?></option>
-                    <?php endforeach ?>
-                </select>
-            </div>
-            <div>
-                <label for="usergroupid">Gebruikergroep</label>
-                <select id="usergroupid" name="usergroupid">
-                    <option value>Iedereen</option>
-                    <?php foreach ($usergroups as $usergroup) : ?>
-                        <option value="<?php echo $usergroup['UserGroupID']; ?>" <?php if (!empty($article['UserGroupID'])) {
-                                                                                        echo $usergroup['UserGroupID'] === $article['UserGroupID'] ? "selected" : "";
-                                                                                    } ?>><?php echo $usergroup['Name'] ?></option>
-                    <?php endforeach ?>
-                </select>
-            </div>
-            <div>
+
+            <div class="edit-area">
                 <textarea id="area_editor" name="content"><?php if (!empty($article['Content'])) {
                                                                 echo $article['Content'];
                                                             } ?></textarea>
             </div>
 
-            <?php if (empty($_GET['id'])) { ?>
-                <button type="submit" name="action" value="create">Artikel Toevoegen</button>
-            <?php } else { ?>
-                <button type="submit" name="action" value="update">Artikel Wijzigen</button>
-                <button type="submit" name="action" value="delete">Artikel Verwijderen</button>
-            <?php } ?>
-            <a href="index.php?page=articles">
-                <div>
-                    <p>Terug</p>
-                </div>
-            </a>
+            <div class="form-grid">
+                <?php if (empty($_GET['id'])) { ?>
+                    <button class="button-yellow button-submit-yellow" type="submit" name="action" value="create">Artikel Toevoegen</button>
+                <?php } else { ?>
+                    <div class="buttons-beheren">
+                        <button class="button-white button-delete" type="submit" name="action" value="delete">Artikel Verwijderen</button>
+                        <button class="button-blue button-submit" type="submit" name="action" value="update">Artikel Wijzigen</button>
+                    </div>
+                <?php } ?>
+
+            </div>
+
         </form>
     </div>
 <?php } ?>
