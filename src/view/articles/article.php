fb163c20370b02021a703266b531347322387f64
Lara Tomme <lara.tomme@gmail.com>
2021-02-04 19:44:11 +0100

    CSS Inhoud pagina #67

--- src/view/articles/article.php
+++ src/view/articles/article.php
@@ -1,21 +1,23 @@
 <!-- Artikel -->
+<h1><?php echo $article['Title'] ?></h1>
+<div>
+    <div><?php echo $article['Content'] ?></div>
+</div>
+
 <?php if (!empty($_SESSION["userData"]) && $_SESSION["userData"]["UserGroupID"] === -1) { ?>
-    <div>
+    <div class="beheer-art-flex">
         <!-- Manage Artikel -->
         <a class="button-aanmaken button-link" href="index.php?page=articles&id=<?php echo $article['ArticleID'] ?>">
             <div class="button-blue">
-                <p>Artikel bewerken</p>
+                <p>Inhoud bewerken</p>
+                <img src="../../assets/img/icons/icon-edit-white.svg" alt="Icoon bewerken folder met potlood">
             </div>
         </a>
         <a class="button-aanmaken button-link" href="index.php?page=articles&action=delete&id=<?php echo $article['ArticleID']; ?>&categoryid=<?php echo $article['CategoryID']; ?>">
             <div class="button-blue">
-                <p>Artikel verwijderen</p>
+                <p>Inhoud verwijderen</p>
+                <img src="../../assets/img/icons/icon-delete-article-white.svg" alt="Icoon verwijderen folder met kruis">
             </div>
         </a>
     </div>
 <?php } ?>
-<h1><?php echo $article['Title'] ?></h1>
-<div>
-    <div><?php echo $article['ArticleID'] ?> - <?php echo $article['Title'] ?></div>
-    <div><?php echo $article['Content'] ?></div>
-</div>
