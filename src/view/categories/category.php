b6a5c6f3e83733f5170526f5b61982d9dc2b34fc
Lara Tomme <lara.tomme@gmail.com>
2021-02-04 11:52:10 +0100

    CSS Beheer buttons op pagina's zelf #61

--- src/view/categories/category.php
+++ src/view/categories/category.php
@@ -1,26 +1,4 @@
 <!-- Onderliggende Categories -->
-<?php if (!empty($_SESSION["userData"]) && $_SESSION["userData"]["UserGroupID"] === -1) { ?>
-    <div>
-        <!-- Manage Categorie -->
-        <a class="button-aanmaken button-link" href="index.php?page=categories&id=<?php echo $category['CategoryID'] ?>">
-            <div class="button-blue">
-                <p>Categorie bewerken</p>
-            </div>
-        </a>
-        <a class="button-aanmaken button-link" href="index.php?page=categories&action=create&parentid=<?php echo $category['CategoryID'] ?>">
-            <div class="button-blue">
-                <p>Sub-Categorie aanmaken</p>
-            </div>
-        </a>
-        <?php if (empty($children) && empty($articles)) { ?>
-            <a class="button-aanmaken button-link" href="index.php?page=categories&action=delete&id=<?php echo $category['CategoryID'] ?>&parentid=<?php echo $category['CategoryParentID'] ?>">
-                <div class="button-blue">
-                    <p>Categorie verwijderen</p>
-                </div>
-            </a>
-        <?php } ?>
-    </div>
-<?php } ?>
 <h1><?php echo $category['Name'] ?></h1>
 <?php if (!empty($children)) { ?>
     <h2 class="header-grey-cat">Categorieën</h2>
@@ -48,18 +26,33 @@
     </div>
 <?php } ?>
 
-<!-- Gelinkte Artikels -->
+<!-- Manage Categorie -->
 <?php if (!empty($_SESSION["userData"]) && $_SESSION["userData"]["UserGroupID"] === -1) { ?>
-    <div>
-        <!-- Manage Artikels -->
-        <a class="button-aanmaken button-link" href="index.php?page=articles&action=create&categoryid=<?php echo $category['CategoryID'] ?>">
+    <div class="beheer-cat-flex">
+        <a class="button-aanmaken button-link" href="index.php?page=categories&id=<?php echo $category['CategoryID'] ?>">
             <div class="button-blue">
-                <p>Artikel aanmaken</p>
+                <p>Categorie bewerken</p>
+                <img src="../../assets/img/icons/icon-edit-white.svg" alt="Icoon bewerken folder met potlood">
             </div>
         </a>
+        <a class="button-aanmaken button-link" href="index.php?page=categories&action=create&parentid=<?php echo $category['CategoryID'] ?>">
+            <div class="button-blue">
+                <p>Sub-Categorie aanmaken</p>
+                <img src="../../assets/img/icons/icon-add-cat-white.svg" alt="Icoon toevoegen folder met plusteken">
+            </div>
+        </a>
+        <?php if (empty($children) && empty($articles)) { ?>
+            <a class="button-aanmaken button-link" href="index.php?page=categories&action=delete&id=<?php echo $category['CategoryID'] ?>&parentid=<?php echo $category['CategoryParentID'] ?>">
+                <div class="button-blue">
+                    <p>Categorie verwijderen</p>
+                    <img src="../../assets/img/icons/icon-delete-cat-white.svg" alt="Icoon verwijderen folder met kruis">
+                </div>
+            </a>
+        <?php } ?>
     </div>
 <?php } ?>
 
+<!-- Gelinkte Artikels -->
 <?php if (!empty($articles)) { ?>
     <h2 class="header-grey-inhoud">Inhoud</h2>
     <div class="inhoud-lijst">
@@ -81,5 +74,17 @@
             </a>
             <div class="inhoud-lijst-lijn"></div>
         <?php endforeach ?>
+    </div>
+<?php } ?>
+
+<!-- Manage Artikels -->
+<?php if (!empty($_SESSION["userData"]) && $_SESSION["userData"]["UserGroupID"] === -1) { ?>
+    <div class="beheer-art-flex">
+        <a class="button-aanmaken button-link" href="index.php?page=articles&action=create&categoryid=<?php echo $category['CategoryID'] ?>">
+            <div class="button-blue">
+                <p>Inhoud aanmaken</p>
+                <img src="../../assets/img/icons/icon-add-article-white.svg" alt="Icoon toevoegen van document met plus teken">
+            </div>
+        </a>
     </div>
 <?php } ?>
